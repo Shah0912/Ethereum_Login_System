9f2a2aabb098c01d5c25423ef08bbfa80064d19a
Shah0912 <adi.shah0912@gmail.com>
2021-09-27 20:30:29 +0530

    debugged the backend.

--- backend/app.js
+++ backend/app.js
@@ -5,29 +5,43 @@ const jwt = require('jsonwebtoken');
 const cuid = require('cuid');
 const cors = require('cors');
 
+console.log("starting...");
+
 const loginContract = LoginContract.at(process.env.LOGIN_CONTRACT_ADDRESS || '0xf7b06365e9012592c8c136b71c7a2475c7a94d71');
 
+// console.log("loginContract = ", loginContract);
+console.log("got logincontract");
 // LoginAttempt is the name of the event that signals logins in the 
+
 // Login contract. This is specified in the login.sol file.
 const loginAttempt = loginContract.LoginAttempt();
+
+console.log("loginAttempt successful");
 
 const challenges = {};
 const successfulLogins = {};
 
+console.log("watching login attempt");
+
 loginAttempt.watch((error, event) => {
+    console.log('login attempt');
     if(error) {
         console.log(error);
         return;
     }
 
-    console.log(event);
+    // console.log(event);
 
     const sender = event.args.sender.toLowerCase();
 
     // If the challenge sent through Ethereum matches the one we generated,
     // mark the login attempt as valid, otherwise ignore it.
+    console.log('challenges sender = ', challenges[sender]);
+    console.log('event.args.challenge = ', event.args.challenge);
+    
     if(challenges[sender] === event.args.challenge) {
         successfulLogins[sender] = true;
+        console.log("successful");
     }
 });
 
@@ -36,9 +50,12 @@ const secret = process.env.JWT_SECRET || "my super secret passcode";
 
 const app = express();
 // WARNING: CHANGE IN PRODUCTION
-app.use(cors({
-    origin: 'http://localhost:9080'
-}))
+// app.use(cors({
+//     origin: 'http://localhost:3000'
+// }))
+app.use(cors());
+// app.use(cor options('*', cors());
+
 app.use(bodyParser.json({ type: () => true }));
 
 function validateJwt(req, res, next) {
@@ -58,11 +75,16 @@ app.post('/login', (req, res) => {
         res.sendStatus(400);
         return;
     }
+    
+    // console.log('app.post req = ', req);
+    console.log("login request");
 
     req.body.address = req.body.address.toLowerCase();
 
     const challenge = cuid();
     challenges[req.body.address] = challenge;
+
+    // console.log("req = ", req);
 
     const token = jwt.sign({ 
         address: req.body.address, 
@@ -73,15 +95,26 @@ app.post('/login', (req, res) => {
         challenge: challenge,
         jwt: token
     });
+
+    // console.log("res = ", res);
+
 });
 
 app.post('/finishLogin', validateJwt, (req, res) => {
+    // console.log("finish login...");
     if(!req.jwt || !req.jwt.address || req.jwt.access !== 'finishLogin') {
         res.sendStatus(400);
+        console.log('req.jwt = ', req.jwt);
+        console.log('req.jwt.address = ', req.jwt.address);
+        console.log('req.jwt.access = ', req.jwt.access);
+        
         return;
     }
 
     if(successfulLogins[req.jwt.address]) {
+        console.log('req.jwt = ', req.jwt);
+        console.log('req.jwt.address = ', req.jwt.address);
+        console.log('req.jwt.access = ', req.jwt.access);
         delete successfulLogins[req.jwt.address];
         delete challenges[req.jwt.address];
 
@@ -89,7 +122,7 @@ app.post('/finishLogin', validateJwt, (req, res) => {
             address: req.jwt.address, 
             access: 'full'
         }, secret);
-
+        console.log("successful login");
         res.json({
             jwt: token,
             address: req.jwt.address
@@ -99,6 +132,8 @@ app.post('/finishLogin', validateJwt, (req, res) => {
         res.sendStatus(202);
     }
 });
+
+
 
 app.post('/apiTest', validateJwt, (req, res) => {
     if(req.jwt.access !== 'full') {
